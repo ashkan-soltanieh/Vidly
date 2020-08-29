54447bd663488e49ae3c81b7ab10fdad0993905c
Ashkan Soltanieh <ashkan98@gmail.com>
2020-08-29 17:32:48 -0600

    Add Membership Type Class

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Vidly.Models
+﻿namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; } //Navigation Property
+        public byte MembershipTypeId { get; set; }
     }
 }
