667ef2ccd83a5b21ea3996606766d282a773a791
Ashkan Soltanieh <ashkan98@gmail.com>
2020-08-31 15:13:29 -0600

    Add Membership Type dropdown to Add New Customer view

--- Vidly/Domain/Customer.cs
+++ Vidly/Domain/Customer.cs
@@ -6,14 +6,20 @@ namespace Vidly.Domain
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         [Range(typeof(DateTime), "1900-01-01", "2099-12-31")]
         [Display(Name = "Date of Birth")]
         public DateTime? BirthDate { get; set; }
+
         public bool IsSubscribedToNewsLetter { get; set; }
-        public MembershipType MembershipType { get; set; } //Navigation Property
+
+        public MembershipType MembershipType { get; set; } //Navigation 
+
+        [Display(Name = "Membership type")]
         public byte MembershipTypeId { get; set; }
     }
 }
